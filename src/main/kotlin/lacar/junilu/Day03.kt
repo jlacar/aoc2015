d7e7d11b9f9689ab05754e69d6594a18f525f458
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-11 14:19:53 -0400

    f - Part 1 passes tests using sample data

--- src/main/kotlin/lacar/junilu/Day03.kt
+++ src/main/kotlin/lacar/junilu/Day03.kt
@@ -1,9 +1,22 @@
 package lacar.junilu
 
-class Day03(private val instructions: String) : Solution<Int>() {
-    override fun part1(): Int {
-//        val x = instructions.fold(listOf(Pair(0, 0))) { acc, char -> ,  }
-        return 0
-    }
+class Day03(private val directions: String) : Solution<Int>() {
+    override fun part1() = housesVisited().distinct().count()
+
+    private fun housesVisited() =
+        directions.fold(mutableListOf(Pair(0, 0))) { acc, char ->
+            acc.apply {
+                add(
+                    when (char) {
+                        '<' -> Pair(last().first - 1, last().second)
+                        '>' -> Pair(last().first + 1, last().second)
+                        '^' -> Pair(last().first, last().second + 1)
+                        else -> Pair(last().first, last().second - 1)
+                    }
+                )
+            }
+        }
+
+
     override fun part2(): Int = 0
 }
--- src/test/kotlin/lacar/junilu/Day03Test.kt
+++ src/test/kotlin/lacar/junilu/Day03Test.kt
@@ -3,7 +3,7 @@ package lacar.junilu
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
 
 private val puzzleInput = readResource("day03")
 
@@ -11,21 +11,16 @@ class Day03Test {
     @Nested
     inner class Samples {
 
-        @Test
-        fun `Part 1`() {
-            val input = listOf(
-                ">" to 2,
-                "^>v<" to 4,
-                "^v^v^v^v^v" to 2
-            )
-
-            input.map { (directions, expectedCount) ->
-                dynamicTest("$directions should visit $expectedCount") {
-                    assertEquals(expectedCount, Day03(directions).part1())
-                }
+        @TestFactory
+        fun `Part 1`() = listOf(
+            ">" to 2,
+            "^>v<" to 4,
+            "^v^v^v^v^v" to 2
+        ).map { (directions, expectedCount) ->
+            dynamicTest("$directions should visit $expectedCount houses") {
+                assertEquals(expectedCount, Day03(directions).part1())
             }
         }
-
     }
 
     @Nested

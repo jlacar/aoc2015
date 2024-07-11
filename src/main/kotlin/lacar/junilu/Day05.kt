270f0e3ba121bd05c030ad3ad7e078154047f6f3
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-11 15:56:29 -0400

    f - Started Day05, Part 1

---
+++ src/main/kotlin/lacar/junilu/Day05.kt
@@ -0,0 +1,11 @@
+package lacar.junilu
+
+class Day05(strings: List<String>) : Solution<Int>() {
+    override fun part1(): Int {
+        return -1
+    }
+
+    override fun part2(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/test/kotlin/lacar/junilu/Day05Test.kt
@@ -0,0 +1,38 @@
+package lacar.junilu
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.TestFactory
+
+class Day05Test {
+    @Nested
+    inner class Samples {
+
+        @TestFactory
+        fun `Part 1 - nice`() = listOf(
+            "ugknbfddgicrmopn",
+            "aaa"
+        ).map { sample ->
+            DynamicTest.dynamicTest("$sample is nice") {
+                assertEquals(1, Day05(listOf(sample)).part1())
+            }
+        }
+
+        @TestFactory
+        fun `Part 1 - naughty`() = listOf(
+            "jchzalrnumimnmhp",
+            "haegwjzuvuyypxyu",
+            "dvszwmarrgswjxmb"
+        ).map { sample ->
+            DynamicTest.dynamicTest("$sample is naughty") {
+                assertEquals(0, Day05(listOf(sample)).part1())
+            }
+        }
+    }
+
+    @Nested
+    inner class Solution {
+
+    }
+}

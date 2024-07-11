aafa93d0b1a58b93c18410093d75ab64be2a2aca
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-10 21:09:05 -0400

    f - start Day 02

---
+++ src/main/kotlin/lacar/junilu/Day02.kt
@@ -0,0 +1,14 @@
+package lacar.junilu
+
+/**
+ * Day 2: I Was Told There Would Be No Math
+ */
+class Day02(override val description: String) : Solution {
+    override fun part1(): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(): Any {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/test/kotlin/lacar/junilu/Day02Test.kt
@@ -0,0 +1,5 @@
+package lacar.junilu
+
+import org.junit.jupiter.api.Assertions.*
+
+class Day02Test

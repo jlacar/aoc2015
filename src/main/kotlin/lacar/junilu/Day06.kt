673e5da8f52cb6eac09e0a13631c928490e233c4
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-11 17:09:45 -0400

    f - Start Day06

---
+++ src/main/kotlin/lacar/junilu/Day06.kt
@@ -0,0 +1,14 @@
+package lacar.junilu
+
+/**
+ * Day 6: Probably a Fire Hazard
+ */
+class Day06(instructions: List<String>) : Solution<Int>{
+    override fun part1(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/test/kotlin/lacar/junilu/Day06Test.kt
@@ -0,0 +1,9 @@
+package lacar.junilu
+
+import org.junit.jupiter.api.Assertions.*
+
+private val puzzleInput = readResource("Day06")
+
+class Day06Test {
+
+}

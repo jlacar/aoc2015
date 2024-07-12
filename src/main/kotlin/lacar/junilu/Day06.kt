f02aba959560767e3f6419b3c4a0b7920bdf240f
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-12 06:34:12 -0400

    f - Solved Day06, Part 1

--- src/main/kotlin/lacar/junilu/Day06.kt
+++ src/main/kotlin/lacar/junilu/Day06.kt
@@ -3,10 +3,42 @@ package lacar.junilu
 /**
  * Day 6: Probably a Fire Hazard
  */
-class Day06(instructions: List<String>) : Solution<Int>{
+class Day06(private val instructions: List<String>) : Solution<Int>() {
+
+    private val lights = Array(1000) { BooleanArray(1000) }
+
     override fun part1(): Int {
-        TODO("Not yet implemented")
+        instructions.forEach { instr -> perform(instr) }
+        return lit(lights)
     }
+
+    private fun perform(instr: String) {
+        val parts = instr.split(" ")
+        val action: (Boolean) -> Boolean = action(parts[0], parts[1])
+        val startPos = pairFrom(parts[parts.lastIndex - 2])
+        val endPos = pairFrom(parts.last())
+
+        for (row in startPos.first .. endPos.first) {
+            for (column in startPos.second..endPos.second) {
+                lights[row][column] = action(lights[row][column])
+            }
+        }
+    }
+
+    private fun pairFrom(s: String): Pair<Int, Int> {
+        val (row, col) = s.split(",")
+        return Pair(row.toInt(), col.toInt())
+    }
+
+    private fun action(action: String, qualifier: String): (Boolean) -> Boolean =
+        when (action) {
+            "toggle" -> { state -> !state }
+            "turn" -> if (qualifier == "on") { _ -> true } else { _ -> false }
+            else -> { state -> state }
+        }
+
+    private fun lit(lights: Array<BooleanArray>) =
+        lights.sumOf { row -> row.count { it } }
 
     override fun part2(): Int {
         TODO("Not yet implemented")
--- src/test/kotlin/lacar/junilu/Day06Test.kt
+++ src/test/kotlin/lacar/junilu/Day06Test.kt
@@ -1,9 +1,43 @@
 package lacar.junilu
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
 
 private val puzzleInput = readResource("Day06")
 
 class Day06Test {
 
+    @Nested
+    inner class Samples {
+        @TestFactory
+        fun `Part 1`() = listOf(
+            "turn on 0,0 through 999,999".lines() to 1_000_000,
+
+            """
+            turn on 499,0 through 500,999
+            turn off 499,499 through 500,500
+            turn off 498,0 through 498,999""".trimIndent().lines()
+            to 1_996,
+
+            "toggle 0,0 through 1,1".lines() to 4,
+
+            "toggle 0,0 through 999,999".lines() to 1_000_000,
+
+        ).map { (input, expected) ->
+            DynamicTest.dynamicTest("Part 1: $input") {
+                assertEquals(expected, Day06(input).part1())
+            }
+        }
+    }
+
+    @Nested
+    inner class Solution {
+        @Test
+        fun `Part 1 - SOLVED`() {
+            assertEquals(543903, Day06(puzzleInput).part1())
+        }
+    }
 }

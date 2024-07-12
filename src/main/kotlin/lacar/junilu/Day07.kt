8908d7f959bcabc6dac9a0c9ad0c9f4565f9a367
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-12 14:05:54 -0400

    f - Implement literal and NOT logic

--- src/main/kotlin/lacar/junilu/Day07.kt
+++ src/main/kotlin/lacar/junilu/Day07.kt
@@ -1,11 +1,39 @@
 package lacar.junilu
 
+import jdk.jfr.Unsigned
+
 class Day07(private val instructions: List<String>) : Solution<Int>() {
+
+    val wires : MutableMap<String, Int> = mutableMapOf("a" to 0)
+
     override fun part1(): Int {
-        TODO("Not yet implemented")
+        instructions.forEach { instruction ->
+            Day7Command(instruction).exec(wires)
+        }
+        return wires["a"]!!
     }
 
     override fun part2(): Int {
         TODO("Not yet implemented")
+    }
+}
+
+const val DAY7_MAX_SIGNAL = 65536
+
+data class Day7Command(val details: String) {
+    val parts = details.split(" -> ")
+    val wire = parts.last()
+    val expression = parts.first()
+
+
+    fun exec(wires : MutableMap<String, Int>) {
+        val parts = expression.split(" ")
+
+        if (parts.first() == "NOT") {
+            wires[wire] = DAY7_MAX_SIGNAL + wires[parts[1]]!!.inv()
+            return
+        }
+
+        wires[wire] = expression.toInt()
     }
 }
--- src/test/kotlin/lacar/junilu/Day07Test.kt
+++ src/test/kotlin/lacar/junilu/Day07Test.kt
@@ -1,13 +1,36 @@
 package lacar.junilu
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.TestFactory
 
 class Day07Test {
 
     @Nested
     inner class Samples {
+        @TestFactory
+        fun `Part 1`() = listOf(
+            """
+            123 -> a
+            433 -> a
+            """.trimIndent().lines() to 433,
 
+            """
+            123 -> x
+            NOT x -> a
+            """.trimIndent().lines() to 65412,
+
+            """
+            456 -> y
+            NOT y -> a
+            """.trimIndent().lines() to 65079
+
+        ).map { (input, expected) ->
+            DynamicTest.dynamicTest("$input -> $expected") {
+                assertEquals(expected, Day07(input).part1())
+            }
+        }
     }
 
     @Nested

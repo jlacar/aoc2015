6c8b97c624a9c492d795ccefc1bd7fae63bf3286
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-12 07:49:02 -0400

    r - Reorganized to eliminate some duplication

--- src/main/kotlin/lacar/junilu/Day06.kt
+++ src/main/kotlin/lacar/junilu/Day06.kt
@@ -2,35 +2,50 @@ package lacar.junilu
 
 import kotlin.math.max
 
+typealias Corner = Pair<Int, Int>
+
 /**
  * Day 6: Probably a Fire Hazard
  */
 class Day06(private val instructions: List<String>) : Solution<Int>() {
+    override fun part1() = executeInstructionsFor(Part1()).howMany()
+    override fun part2() = executeInstructionsFor(Part2()).howMany()
 
-    private val lights = Array(1000) { BooleanArray(1000) }
-    private val brights = Array(1000) { IntArray(1000) }
-
-    override fun part1(): Int {
-        instructions.forEach { instr -> perform(instr) }
-        return lit(lights)
+    private fun executeInstructionsFor(lights: Day06Part): Day06Part {
+        instructions.forEach { instr ->
+            val parts = instr.split(" ")
+            val action = parts[0]
+            val qualifier = parts[1]
+            val topCorner = toCorner(parts[parts.lastIndex - 2])
+            val bottomCorner = toCorner(parts.last())
+            lights.perform(action, qualifier, topCorner, bottomCorner)
+        }
+        return lights
     }
+}
 
-    private fun perform(instr: String) {
-        val parts = instr.split(" ")
-        val action: (Boolean) -> Boolean = action(parts[0], parts[1])
-        val startPos = pairFrom(parts[parts.lastIndex - 2])
-        val endPos = pairFrom(parts.last())
+interface Day06Part {
+    fun perform(command: String, qualifier: String, topCorner: Corner, bottomCorner: Corner)
+    fun howMany(): Int
+}
 
-        for (row in startPos.first..endPos.first) {
-            for (column in startPos.second..endPos.second) {
+private fun toCorner(s: String): Corner {
+    val (row, col) = s.split(",")
+    return Pair(row.toInt(), col.toInt())
+}
+
+private class Part1 : Day06Part {
+    private val lights = Array(1000) { BooleanArray(1000) }
+
+    override fun howMany(): Int = lights.sumOf { row -> row.count { it } }
+
+    override fun perform(command: String, qualifier: String, topCorner: Corner, bottomCorner: Corner) {
+        val action = action(command, qualifier)
+        for (row in topCorner.first..bottomCorner.first) {
+            for (column in topCorner.second..bottomCorner.second) {
                 lights[row][column] = action(lights[row][column])
             }
         }
-    }
-
-    private fun pairFrom(s: String): Pair<Int, Int> {
-        val (row, col) = s.split(",")
-        return Pair(row.toInt(), col.toInt())
     }
 
     private fun action(action: String, qualifier: String): (Boolean) -> Boolean = when (action) {
@@ -38,28 +53,23 @@ class Day06(private val instructions: List<String>) : Solution<Int>() {
         "turn" -> if (qualifier == "on") { _ -> true } else { _ -> false }
         else -> { state -> state }
     }
+}
 
-    private fun lit(lights: Array<BooleanArray>) = lights.sumOf { row -> row.count { it } }
+private class Part2 : Day06Part {
+    private val brights = Array(1000) { IntArray(1000) }
 
-    override fun part2(): Int {
-        instructions.forEach { instr -> perform2(instr) }
-        return brights.sumOf { row -> row.sumOf { it } }
-    }
+    override fun howMany(): Int = brights.sumOf { row -> row.sumOf { it } }
 
-    private fun perform2(instr: String) {
-        val parts = instr.split(" ")
-        val action: (Int) -> Int = action2(parts[0], parts[1])
-        val startPos = pairFrom(parts[parts.lastIndex - 2])
-        val endPos = pairFrom(parts.last())
-
-        for (row in startPos.first..endPos.first) {
-            for (column in startPos.second..endPos.second) {
+    override fun perform(command: String, qualifier: String, topCorner: Corner, bottomCorner: Corner) {
+        val action = action(command, qualifier)
+        for (row in topCorner.first..bottomCorner.first) {
+            for (column in topCorner.second..bottomCorner.second) {
                 brights[row][column] = action(brights[row][column])
             }
         }
     }
 
-    private fun action2(action: String, qualifier: String): (Int) -> Int = when (action) {
+    private fun action(action: String, qualifier: String): (Int) -> Int = when (action) {
         "toggle" -> { intensity -> intensity + 2 }
         "turn" -> if (qualifier == "on") { i -> i + 1 } else { i -> max(i - 1, 0) }
         else -> { state -> state }

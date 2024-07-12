f2d343993e16321c2590a87e0d462ff5ab3925c0
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-12 11:17:10 -0400

    f - Started Day07

---
+++ src/main/kotlin/lacar/junilu/Day07.kt
@@ -0,0 +1,11 @@
+package lacar.junilu
+
+class Day07(private val instructions: List<String>) : Solution<Int>() {
+    override fun part1(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/test/kotlin/lacar/junilu/Day07Test.kt
@@ -0,0 +1,17 @@
+package lacar.junilu
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
+
+class Day07Test {
+
+    @Nested
+    inner class Samples {
+
+    }
+
+    @Nested
+    inner class Solutions {
+
+    }
+}

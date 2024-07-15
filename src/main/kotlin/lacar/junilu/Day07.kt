f219d10f87909f09ee60d3d5de9a92af08ef5b4e
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-15 10:12:33 -0400

    f - Solved Day07, Parts 1 and 2

--- src/main/kotlin/lacar/junilu/Day07.kt
+++ src/main/kotlin/lacar/junilu/Day07.kt
@@ -1,39 +1,97 @@
 package lacar.junilu
 
-import jdk.jfr.Unsigned
+import java.rmi.UnexpectedException
 
 class Day07(private val instructions: List<String>) : Solution<Int>() {
 
-    val wires : MutableMap<String, Int> = mutableMapOf("a" to 0)
+    private val circuit = Circuit.assemble(instructions)
 
     override fun part1(): Int {
-        instructions.forEach { instruction ->
-            Day7Command(instruction).exec(wires)
-        }
-        return wires["a"]!!
+        memo.clear()
+        return circuit.signalTo("a") ?: throw UnexpectedException("Something's wrong!")
     }
 
     override fun part2(): Int {
-        TODO("Not yet implemented")
+        val a = part1()
+
+        memo.clear()
+        return Circuit(replaceSegmentB(circuit, a)).signalTo("a") ?: throw UnexpectedException("Something's wrong!")
+    }
+
+    private fun replaceSegmentB(circuit: Circuit, a: Int): Map<String, SignalProvider> {
+        val newSegments = mutableMapOf<String, SignalProvider>().apply { putAll(circuit.segments) }
+        newSegments["b"] = Value("b", a.toString(), Op.ASSIGN)
+        return newSegments
     }
 }
 
-const val DAY7_MAX_SIGNAL = 65536
+private val memo = object {
+    private val cache = mutableMapOf<String, Int?>()
 
-data class Day7Command(val details: String) {
-    val parts = details.split(" -> ")
-    val wire = parts.last()
-    val expression = parts.first()
+    fun clear() = cache.clear()
 
+    fun signalTo(id: String, calc: (String) -> Int?): Int? {
+        if (cache[id] == null) cache[id] = calc(id)
+        return cache[id]
+    }
+}
 
-    fun exec(wires : MutableMap<String, Int>) {
-        val parts = expression.split(" ")
+private typealias SegmentMap = Map<String, SignalProvider>
 
-        if (parts.first() == "NOT") {
-            wires[wire] = DAY7_MAX_SIGNAL + wires[parts[1]]!!.inv()
-            return
+private sealed interface SignalProvider {
+    val name: String
+    fun output(segments: SegmentMap): Int?
+}
+
+private enum class Op {
+    ASSIGN { override fun eval(a: Int, b: Int): Int = a },
+    NOT { override fun eval(a: Int, b: Int): Int = a.inv() and 0xffff },    // mask to 16-bit value
+    AND { override fun eval(a: Int, b: Int): Int = a and b },
+    OR { override fun eval(a: Int, b: Int): Int = a or b },
+    LSHIFT { override fun eval(a: Int, b: Int): Int = a shl b },
+    RSHIFT { override fun eval(a: Int, b: Int): Int = a shr b };
+
+    abstract fun eval(a: Int, b: Int = 0): Int
+}
+
+/** Extension function to check if value is a numeric literal */
+private fun String.isNumberLiteral(): Boolean = first().isDigit()
+
+private class Value(override val name: String, val input: String, val op: Op) : SignalProvider {
+    override fun output(segments: SegmentMap): Int? =
+        if (input.isNumberLiteral()) input.toInt()
+        else memo.signalTo(input) { id -> segments[id]?.output(segments) }
+}
+
+private class Gate(override val name: String, val inputA: String, val inputB: String, val op: Op) : SignalProvider {
+    override fun output(segments: SegmentMap): Int? {
+        val calc: (String) -> Int? = { id -> segments[id]?.output(segments) }
+        val valueA = if (inputA.isNumberLiteral()) inputA.toInt() else memo.signalTo(inputA, calc)
+        val valueB = if (inputB.isNumberLiteral()) inputB.toInt() else memo.signalTo(inputB, calc)
+        return if (valueA == null || valueB == null) null else op.eval(valueA, valueB)
+    }
+}
+
+private class Circuit(val segments: SegmentMap) {
+
+    fun signalTo(id: String): Int? = segments[id]?.output(segments)
+
+    companion object {
+        private const val NOT_USED = "_"
+        fun assemble(instructions: List<String>): Circuit {
+            val segments = mutableMapOf<String, SignalProvider>()
+            segments[NOT_USED] = Value("0", "0", Op.ASSIGN)
+            instructions.forEach { instruction ->
+                val parts = instruction.split(" ")
+                val segment: SignalProvider = when (parts.size) {
+                    3 -> Value(name = parts.last(), parts.first(), Op.ASSIGN)                   // ? -> wire
+                    4 -> Gate(name = parts.last(), parts[1], NOT_USED, Op.NOT)                  // NOT wire -> wire
+                    5 -> Gate(name = parts.last(), parts[0], parts[2], Op.valueOf(parts[1]))    // a OP b -> wire
+                    else -> throw UnsupportedOperationException("Unsupported instruction: $instruction")
+                }
+                segments[segment.name] = segment
+            }
+            return Circuit(segments)
         }
-
-        wires[wire] = expression.toInt()
     }
 }
--- src/test/kotlin/lacar/junilu/Day07Test.kt
+++ src/test/kotlin/lacar/junilu/Day07Test.kt
@@ -3,7 +3,10 @@ package lacar.junilu
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
+
+private val puzzleInput = readResource("day07")
 
 class Day07Test {
 
@@ -13,20 +16,56 @@ class Day07Test {
         fun `Part 1`() = listOf(
             """
             123 -> a
-            433 -> a
-            """.trimIndent().lines() to 433,
+            """.trimIndent().lines() to 123,
 
             """
+            x -> a
             123 -> x
+            """.trimIndent().lines() to 123,
+
+            """
             NOT x -> a
+            123 -> x
             """.trimIndent().lines() to 65412,
 
             """
-            456 -> y
             NOT y -> a
-            """.trimIndent().lines() to 65079
+            456 -> y
+            """.trimIndent().lines() to 65079,
 
-        ).map { (input, expected) ->
+            """
+            x AND y -> a
+            123 -> x
+            456 -> y
+            """.trimIndent().lines() to 72,
+
+            """
+            1 AND x -> a
+            123 -> x
+            """.trimIndent().lines() to 1,
+
+            """
+            1 AND x -> a
+            456 -> x
+            """.trimIndent().lines() to 0,
+
+            """
+            x OR y -> a
+            123 -> x
+            456 -> y
+            """.trimIndent().lines() to 507,
+
+            """
+            x LSHIFT 2 -> a
+            123 -> x
+            """.trimIndent().lines() to 492,
+
+            """
+            y RSHIFT 2 -> a
+            456 -> y
+            """.trimIndent().lines() to 114,
+
+            ).map { (input, expected) ->
             DynamicTest.dynamicTest("$input -> $expected") {
                 assertEquals(expected, Day07(input).part1())
             }
@@ -36,5 +75,14 @@ class Day07Test {
     @Nested
     inner class Solutions {
 
+        @Test
+        fun `Part 1 - SOLVED`() {
+            assertEquals(3176, Day07(puzzleInput).part1())
+        }
+
+        @Test
+        fun `Part 2 - SOLVED`() {
+            assertEquals(14710, Day07(puzzleInput).part2())
+        }
     }
 }

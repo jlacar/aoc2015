67de908dfbf20b2d44870c8e334991515549c4a3
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-16 17:44:55 -0400

    r - Inline expressions and simplified calculation of next house using data class copy() and named parameter

--- src/main/kotlin/lacar/junilu/Day03.kt
+++ src/main/kotlin/lacar/junilu/Day03.kt
@@ -7,27 +7,18 @@ class Day03(private val directions: String) : Solution<Int>() {
     override fun part1() = housesVisited(directions).distinct().count()
 
     private fun housesVisited(directions: String) =
-        directions.fold(mutableListOf(Pair(0, 0))) { acc, char ->
-            acc.apply {
-                add(
-                    when (char) {
-                        '<' -> Pair(last().first - 1, last().second)
-                        '>' -> Pair(last().first + 1, last().second)
-                        '^' -> Pair(last().first, last().second + 1)
-                        else -> Pair(last().first, last().second - 1)
-                    }
-                )
+        directions.runningFold(Pair(0, 0)) { currentHouse, ch ->
+            when (ch) {
+                '<' -> currentHouse.copy(first = currentHouse.first - 1)
+                '>' -> currentHouse.copy(first = currentHouse.first + 1)
+                '^' -> currentHouse.copy(second = currentHouse.second + 1)
+                else -> currentHouse.copy(second = currentHouse.second - 1)
             }
         }
 
-    override fun part2(): Int {
-        val (santaDirections, roboSantaDirections) = splitUp(directions)
-        return (housesVisited(santaDirections) + housesVisited(roboSantaDirections)).distinct().count()
-    }
-
-    private fun splitUp(directions: String): Pair<String, String> =
-        Pair(
-            directions.filterIndexed { index, _ -> index % 2 == 0 },
-            directions.filterIndexed { index, _ -> index % 2 == 1 }
-        )
+    override fun part2() =
+        (housesVisited(directions.filterIndexed { index, _ -> index % 2 == 0 }) +
+         housesVisited(directions.filterIndexed { index, _ -> index % 2 == 1 }))
+        .distinct()
+        .count()
 }

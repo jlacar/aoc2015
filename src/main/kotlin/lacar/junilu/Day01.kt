3365b1f894c11f517cd58e5bdfc08104263ae34a
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-15 20:59:12 -0400

    r - Reorder for reading order

--- src/main/kotlin/lacar/junilu/Day01.kt
+++ src/main/kotlin/lacar/junilu/Day01.kt
@@ -6,10 +6,10 @@ package lacar.junilu
 class Day01(private val input: String) : Solution<Int>() {
     override fun part1() = input.count { it == '(' } - input.count { it == ')' }
 
-    private fun delta(c: Char) = if (c == '(') 1 else -1
-
     override fun part2() =
         input
             .runningFold(0) { acc: Int, c: Char -> acc + delta(c) }
             .indexOfFirst { it == -1 }
+
+    private fun delta(c: Char) = if (c == '(') 1 else -1
 }

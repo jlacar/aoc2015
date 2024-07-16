ebfb5774a346f13be02dfda4274d9be98fc20162
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-15 20:29:56 -0400

    f - Started Day09

---
+++ src/main/kotlin/lacar/junilu/Day09.kt
@@ -0,0 +1,22 @@
+package lacar.junilu
+
+class Day09(distances: List<Day9Segment>): Solution<Int>() {
+    override fun part1(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(): Int {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        fun parseDistances(input: List<String>) = Day09(
+            input.map { line ->
+                val (startCity, _, endCity, _, distance) = line.split(" ", " = ")
+                Day9Segment(startCity, endCity, distance.toInt())
+            }
+        )
+    }
+}
+
+data class Day9Segment(val startCity: String, val endCity: String, val distance: Int)

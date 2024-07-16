71adbdebf26199543579f7a9daa8cdbdb583525d
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-16 17:14:22 -0400

    r - Alternative solution using fold()

--- src/main/kotlin/lacar/junilu/Day01.kt
+++ src/main/kotlin/lacar/junilu/Day01.kt
@@ -4,11 +4,9 @@ package lacar.junilu
  * AoC 2015 - Day 1: Not Quite Lisp
  */
 class Day01(private val input: String) : Solution<Int>() {
-    override fun part1() = input.count { it == '(' } - input.count { it == ')' }
+    override fun part1() = input.fold(0) { floor, c -> floor + if (c == '(') 1 else -1 }
 
     override fun part2() = input
-            .runningFold(0) { acc: Int, c: Char -> acc + delta(c) }
+            .runningFold(0) { floor, c -> floor + if (c == '(') 1 else -1 }
             .indexOfFirst { it == -1 }
-
-    private fun delta(c: Char) = if (c == '(') 1 else -1
 }
--- src/main/kotlin/lacar/junilu/Day09.kt
+++ src/main/kotlin/lacar/junilu/Day09.kt
@@ -1,25 +1,41 @@
-package lacar.junilu
-
-class Day09(val distances: List<Day9Segment>): Solution<Int>() {
-    override fun part1(): Int {
-//        val cities = distances.mapTo(mutableSetOf<String>()) {
+//package lacar.junilu
 //
-//        }
-        TODO("Not yet implemented")
-    }
-
-    override fun part2(): Int {
-        TODO("Not yet implemented")
-    }
-
-    companion object {
-        fun parseDistances(input: List<String>) = Day09(
-            input.map { line ->
-                val (startCity, _, endCity, _, distance) = line.split(" ", " = ")
-                Day9Segment(startCity, endCity, distance.toInt())
-            }
-        )
-    }
-}
-
-data class Day9Segment(val startCity: String, val endCity: String, val distance: Int)
+//class Day09(val graph: List<Day9Segment>): Solution<Int>() {
+//    fun permutationsOf(cities: Set<String>): List<List<String>> {
+//        return listOf()
+//    }
+//
+//    override fun part1(): Int {
+//        TODO("Not yet implemented")
+////        val cities = mutableSetOf<String>()
+////        graph.forEach { segment -> with(cities) { add(segment.startCity); add(segment.endCity) }
+////        permutationsOf(cities).windowed(2).map { (startCity, endCity) -> graph }
+//    }
+//
+//    override fun part2(): Int {
+//        TODO("Not yet implemented")
+//    }
+//
+//    companion object {
+//        fun using(input: List<String>) = Day09(
+//            input.map { line ->
+//                val (startCity, _, endCity, _, distance) = line.split(" ", " = ")
+//                Day9Segment(startCity, endCity, distance.toInt())
+//            }
+//        )
+//    }
+//}
+//
+//data class Day9Segment(val startCity: String, val endCity: String, val distance: Int) {
+//    override fun equals(other: Any?): Boolean {
+//        if (this === other) return true
+//        if (other == null || javaClass != other.javaClass) return false
+//        other as Day9Segment
+//        return startCity == other.startCity && endCity == other.endCity ||
+//               startCity == other.endCity && endCity == other.startCity
+//    }
+//
+//    override fun hashCode(): Int =
+//        31 * (startCity.hashCode() + endCity.hashCode()) + distance
+//    }
+//}

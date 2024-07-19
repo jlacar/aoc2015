e69a69b7b5ef67029cfb70f26709847050366c69
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-19 16:13:04 -0400

    r - General refactoring, convert to single-line functions, consolidating functions, rename a bunch of things.

--- src/main/kotlin/lacar/junilu/Day09.kt
+++ src/main/kotlin/lacar/junilu/Day09.kt
@@ -6,41 +6,29 @@ private typealias SantaRouteSegment = Pair<String, Int>
 
 class Day09(private val segments: List<SantaRouteSegment>) : Solution<Int>() {
 
-    override fun part1() = allPathsThrough(citiesIn(segments))
-        .minOfOrNull { eachPath -> distanceThrough(eachPath) } ?: 0
+    override fun part1() = allPossiblePaths().minOf { eachPath -> distanceThrough(eachPath) }
 
-    override fun part2() = allPathsThrough(citiesIn(segments))
-        .maxOfOrNull { eachPath -> distanceThrough(eachPath) } ?: 0
-
-    private fun allPathsThrough(cities: Set<String>): List<List<String>> = permutations(cities.toList())
+    override fun part2() = allPossiblePaths().maxOf { eachPath -> distanceThrough(eachPath) }
 
     private fun distanceThrough(cities: List<String>) = cities.windowed(2)
         .sumOf { (city1, city2) ->
             segments.find { it.first == "$city1 to $city2" || it.first == "$city2 to $city1" }?.second ?: 0
         }
 
-    private fun permutations(input: List<String>): List<List<String>> {
-        val solutions = mutableListOf<List<String>>()
-        permutationsRecursive(input, 0, solutions)
-        return solutions
+    private fun allCities() = segments.flatMap { it.first.split(" to ").toList() }.distinct()
+
+    private fun allPossiblePaths(): List<List<String>> {
+        val paths = mutableListOf<List<String>>()
+        permutationsOf(allCities(), 0, paths)
+        return paths
     }
 
-    private fun permutationsRecursive(input: List<String>, index: Int, answers: MutableList<List<String>>) {
-        if (index == input.lastIndex) answers.add(input.toList())
-        for (i in index..input.lastIndex) {
-            swap(input, index, i)
-            permutationsRecursive(input, index + 1, answers)
-            swap(input, i, index)
-        }
-    }
-
-    private fun citiesIn(segments: List<SantaRouteSegment>): Set<String> {
-        return segments.fold(mutableSetOf()) { cities, segment ->
-            cities.apply {
-                val (city1, city2) = segment.first.split(" to ")
-                add(city1)
-                add(city2)
-            }
+    private fun permutationsOf(cities: List<String>, index: Int, soFar: MutableList<List<String>>) {
+        if (index == cities.lastIndex) soFar.add(cities.toList())
+        for (i in index..cities.lastIndex) {
+            swap(cities, index, i)
+            permutationsOf(cities, index + 1, soFar)
+            swap(cities, i, index)
         }
     }
 
@@ -50,5 +38,4 @@ class Day09(private val segments: List<SantaRouteSegment>) : Solution<Int>() {
             SantaRouteSegment(cities, distance.toInt())
         })
     }
-
 }

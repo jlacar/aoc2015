08ca5343258595b84dafbc0d2ec5dcf58a623d91
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-20 07:41:06 -0400

    r - Remove type declarations that can be inferred

--- src/main/kotlin/lacar/junilu/Day10.kt
+++ src/main/kotlin/lacar/junilu/Day10.kt
@@ -7,7 +7,7 @@ class Day10(val input: String = "3113322113") : Solution<Int>() {
     override fun part1(): Int = lookSay(40)
     override fun part2(): Int = lookSay(50)
 
-    private fun lookSay(n: Int): Int = (1..n).fold(input) { look: String, _: Int -> say(look) }.length
+    private fun lookSay(n: Int) = (1..n).fold(input) { look, _ -> say(look) }.length
 
     private val digitSequences = """(.)\1*""".toRegex()
 
@@ -17,21 +17,3 @@ class Day10(val input: String = "3113322113") : Solution<Int>() {
         .map { sequence -> "${sequence.length}${sequence.first()}" }
         .joinToString("")
 }
-
-/**
- * Ugly implementation
-
-//        val firstDigit = input.first()
-//        val sequences = input.drop(1).fold(mutableListOf(firstDigit.toString())) { acc, c ->
-//            if (acc.last().startsWith(c)) {
-//                val seq = acc.removeLast()
-//                acc.add(seq + c)
-//            } else {
-//                acc.add(c.toString())
-//            }
-//            acc
-//        }
-//        // .also { println(it) }
-//        return sequences.map { "${it.length}${it.first()}"}.joinToString("")
-
- */

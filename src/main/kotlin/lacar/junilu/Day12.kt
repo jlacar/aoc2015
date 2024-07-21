cdb8264d083d623222fcfc5bdaf1d83dd2e7ba22
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-20 20:14:51 -0400

    f - Pass Day12, Part 2 sample tests

--- src/main/kotlin/lacar/junilu/Day12.kt
+++ src/main/kotlin/lacar/junilu/Day12.kt
@@ -4,10 +4,13 @@ class Day12(val input: String) : Solution<Int>() {
 
     val numbers = """-?\d+""".toRegex()
 
-    override fun part1() =
-        numbers.findAll(input).sumOf { matchResult -> matchResult.groupValues.first().toInt() }
+    override fun part1() = sumNumbers(input)
 
-    override fun part2(): Int {
-        TODO("Not yet implemented")
-    }
+    private fun sumNumbers(input: String) = numbers.findAll(input)
+        .sumOf { matchResult -> matchResult.groupValues.first().toInt() }
+
+    val removeRedObjects = """\{.*"red".*}""".toRegex()
+
+    override fun part2(): Int =
+        removeRedObjects.replace(input, "").let { sumNumbers(it) }
 }
--- src/test/kotlin/lacar/junilu/Day12Test.kt
+++ src/test/kotlin/lacar/junilu/Day12Test.kt
@@ -24,6 +24,18 @@ class Day12Test {
                 assertEquals(expectedOutput, Day12(input).part1())
             }
         }
+
+        @TestFactory
+        fun `Part 2`() = listOf(
+            """[1,{"c":"red","b":2},3]""" to 4,
+            """[1,2,3]""" to 6,
+            """[1,"red",5]""" to 6,
+            """{"d":"red","e":[1,2,3,4],"f":5}""" to 0,
+        ).map { (input, expectedOutput) ->
+            DynamicTest.dynamicTest("$input -> $expectedOutput") {
+                assertEquals(expectedOutput, Day12(input).part2())
+            }
+        }
     }
 
     @Nested
@@ -31,6 +43,10 @@ class Day12Test {
         @Test
         fun `Part 1 - SOLVED`() {
             assertEquals(111754, Day12(readResource("day12").first()).part1())
+        }
+        @Test
+        fun `Part 2 - SOLVED`() {
+            assertEquals(1, Day12(readResource("day12").first()).part2())
         }
     }
 }

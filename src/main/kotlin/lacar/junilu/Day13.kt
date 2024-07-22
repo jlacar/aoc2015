5cd4e615cd7b32719bbd90159b93776d3f5d511f
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-22 19:56:16 -0400

    f - Started Day13

---
+++ src/main/kotlin/lacar/junilu/Day13.kt
@@ -0,0 +1,27 @@
+package lacar.junilu
+
+/**
+ * AoC 2015 - Day 13: Knights of the Dinner Table
+ */
+class Day13(input: Map<String, Int>) : Solution<Int>() {
+    override fun part1(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(): Int {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        fun using(input: List<String>) = Day13(
+            input.map { line ->
+                val (name1, _, sign, units, name2) = line
+                    .replace("happiness units by sitting next to ", "")
+                    .split(" ")
+                "$name1 to $name2" to (if (sign == "gain") 1 else -1) * units.toInt()
+            }.toMap()
+        )
+
+        fun peopleIn(input: List<String>) = input.map { it.split(" ").first() }.distinct()
+    }
+}
---
+++ src/test/kotlin/lacar/junilu/Day13Test.kt
@@ -0,0 +1,46 @@
+package lacar.junilu
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+private val puzzleInput = readResource("day13")
+
+class Day13Test {
+    @Nested
+    inner class Samples {
+        @Test
+        fun `Part 1`() {
+            val input =
+                """
+                Alice would gain 54 happiness units by sitting next to Bob.
+                Alice would lose 79 happiness units by sitting next to Carol.
+                Alice would lose 2 happiness units by sitting next to David.
+                Bob would gain 83 happiness units by sitting next to Alice.
+                Bob would lose 7 happiness units by sitting next to Carol.
+                Bob would lose 63 happiness units by sitting next to David.
+                Carol would lose 62 happiness units by sitting next to Alice.
+                Carol would gain 60 happiness units by sitting next to Bob.
+                Carol would gain 55 happiness units by sitting next to David.
+                David would gain 46 happiness units by sitting next to Alice.
+                David would lose 7 happiness units by sitting next to Bob.
+                David would gain 41 happiness units by sitting next to Carol.                
+                """.trimIndent().lines()
+
+            assertEquals(330, Day13.using(input).part1())
+        }
+    }
+
+    @Nested
+    inner class Solution {
+        @Test
+        fun `Part 1 - `() {
+            assertEquals(0, Day13.using(puzzleInput).part1())
+        }
+
+        @Test
+        fun `Part 2 - `() {
+            assertEquals(1, Day13.using(puzzleInput).part2())
+        }
+    }
+}

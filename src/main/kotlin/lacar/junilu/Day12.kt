5ab05a41800172e11bb30edc452441f09a6c8ef8
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-23 11:01:32 -0400

    r - Add explaining variable and rename to deepSumOfNumbersIn() to make the code more expressive

--- src/main/kotlin/lacar/junilu/Day12.kt
+++ src/main/kotlin/lacar/junilu/Day12.kt
@@ -6,17 +6,19 @@ import kotlinx.serialization.json.*
  * AoC 2015 - Day 12: JSAbacusFramework.io
  */
 class Day12(val input: String) : Solution<Int>() {
-    override fun part1() = deepSumOf(Json.decodeFromString<JsonElement>(input))
+    private val json = Json.decodeFromString<JsonElement>(input)
 
-    override fun part2() = deepSumOf(Json.decodeFromString<JsonElement>(input)) { element ->
+    override fun part1() = deepSumOfNumbersIn(json)
+
+    override fun part2() = deepSumOfNumbersIn(json) { element ->
         element.values.any() { it == JsonPrimitive("red") }
     }
 }
 
-private fun deepSumOf(element: JsonElement, skip: (JsonObject) -> Boolean = { false }): Int =
+private fun deepSumOfNumbersIn(element: JsonElement, skip: (JsonObject) -> Boolean = { false }): Int =
     when (element) {
         is JsonPrimitive -> element.intOrNull ?: 0
-        is JsonObject -> if (skip(element)) 0 else element.values.sumOf { deepSumOf(it, skip) }
-        is JsonArray -> element.sumOf { deepSumOf(it, skip) }
+        is JsonObject -> if (skip(element)) 0 else element.values.sumOf { deepSumOfNumbersIn(it, skip) }
+        is JsonArray -> element.sumOf { deepSumOfNumbersIn(it, skip) }
         else -> 0
     }

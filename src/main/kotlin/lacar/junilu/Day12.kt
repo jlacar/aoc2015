9ccd750578d0e404d6b4d002cc764667c12fb2aa
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-23 08:50:11 -0400

    r - Convert Day12, Part 1 to use deepSumOf instead of regular expression

--- src/main/kotlin/lacar/junilu/Day12.kt
+++ src/main/kotlin/lacar/junilu/Day12.kt
@@ -6,16 +6,18 @@ import kotlinx.serialization.json.*
  * AoC 2015 - Day 12: JSAbacusFramework.io
  */
 class Day12(val input: String) : Solution<Int>() {
-    override fun part1() = """-?\d+""".toRegex().findAll(input)
-        .sumOf { matchResult -> matchResult.groupValues.first().toInt() }
+    override fun part1() = deepSumOf(Json.decodeFromString<JsonElement>(input))
 
-    override fun part2() = deepSumOf(Json.decodeFromString<JsonElement>(input))
+    override fun part2() = deepSumOf(Json.decodeFromString<JsonElement>(input)) { element ->
+        element.jsonObject.containsValue(JsonPrimitive("red"))
+    }
 }
 
-private fun deepSumOf(element: JsonElement): Int = when (element) {
+private fun deepSumOf(element: JsonElement, skip: (JsonElement) -> Boolean =
+    { _ -> false }): Int = when (element) {
     is JsonPrimitive -> element.intOrNull ?: 0
-    is JsonObject -> if (element.containsValue(JsonPrimitive("red"))) 0
-                     else element.values.sumOf { deepSumOf(it) }
-    is JsonArray -> element.sumOf { deepSumOf(it) }
+    is JsonObject -> if (skip.invoke(element)) 0
+                     else element.values.sumOf { deepSumOf(it, skip) }
+    is JsonArray -> element.sumOf { deepSumOf(it, skip) }
     else -> 0
 }

eb8c4b413e98adaca8d48f076b19da71b2d74ac1
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-23 09:51:10 -0400

    r - Change type from JsonElement to JsonObject and make direct call instead of calling invoke()

--- src/main/kotlin/lacar/junilu/Day12.kt
+++ src/main/kotlin/lacar/junilu/Day12.kt
@@ -9,15 +9,14 @@ class Day12(val input: String) : Solution<Int>() {
     override fun part1() = deepSumOf(Json.decodeFromString<JsonElement>(input))
 
     override fun part2() = deepSumOf(Json.decodeFromString<JsonElement>(input)) { element ->
-        element.jsonObject.containsValue(JsonPrimitive("red"))
+        element.values.any() { it == JsonPrimitive("red") }
     }
 }
 
-private fun deepSumOf(element: JsonElement, skip: (JsonElement) -> Boolean =
-    { _ -> false }): Int = when (element) {
-    is JsonPrimitive -> element.intOrNull ?: 0
-    is JsonObject -> if (skip.invoke(element)) 0
-                     else element.values.sumOf { deepSumOf(it, skip) }
-    is JsonArray -> element.sumOf { deepSumOf(it, skip) }
-    else -> 0
-}
+private fun deepSumOf(element: JsonElement, skip: (JsonObject) -> Boolean = { _ -> false }): Int =
+    when (element) {
+        is JsonPrimitive -> element.intOrNull ?: 0
+        is JsonObject -> if (skip(element)) 0 else element.values.sumOf { deepSumOf(it, skip) }
+        is JsonArray -> element.sumOf { deepSumOf(it, skip) }
+        else -> 0
+    }

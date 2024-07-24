2a064a8147f7bf66fa73cbaf43ac9d29a62d77ec
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-24 07:33:51 -0400

    r - Move race by point logic out of data class and into its own encapsulated function

--- src/main/kotlin/lacar/junilu/Day14.kt
+++ src/main/kotlin/lacar/junilu/Day14.kt
@@ -9,17 +9,19 @@ class Day14(private val reindeerStats: List<ReindeerStats>, private val raceTime
 
     override fun part1() = reindeerStats.maxOf { it.distanceFlownIn(raceTime) }
 
-    override fun part2(): Int {
-        raceByPointSystem()
-        return reindeerStats.maxOf { it.score }
-    }
+    override fun part2() = racePoints().maxOf { it.value }
 
-    private fun raceByPointSystem() {
-        reindeerStats.forEach { it.resetScore() }
-        (1..raceTime).forEach { raceSecond ->
-            val maxDistance = reindeerStats.maxOf { it.distanceFlownIn(raceSecond) }
-            reindeerStats.forEach { it.score(maxDistance, raceSecond) }
+    private fun racePoints(): Map<ReindeerStats, Int> {
+        val points = reindeerStats.associateWith { 0 }.toMutableMap()
+        (1..raceTime).forEach { second ->
+            val leadDistance = reindeerStats.maxOf { it.distanceFlownIn(second) }
+            points.forEach { (reindeerStat, currentScore) ->
+                if (reindeerStat.distanceFlownIn(second) >= leadDistance) {
+                    points[reindeerStat] = currentScore + 1
+                }
+            }
         }
+        return points
     }
 
     companion object {
@@ -33,16 +35,7 @@ class Day14(private val reindeerStats: List<ReindeerStats>, private val raceTime
     }
 
     data class ReindeerStats(val speed: Int, val flightTime: Int, val restTime: Int) {
-
         private val cycleTime = flightTime + restTime
-
-        var score = 0
-
-        fun resetScore() { score = 0 }
-
-        fun score(distanceToBeat: Int, raceTime: Int) {
-            if (distanceFlownIn(raceTime) >= distanceToBeat) score++
-        }
 
         fun distanceFlownIn(raceTime: Int) = speed * secondsFlyingDuring(raceTime)
 

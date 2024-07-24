cadb82b7b79f51590219fa99fae4057d42272fc3
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-24 07:07:51 -0400

    r - Move bulk of Part 2 race logic to data class. Also renamed a few things.

--- src/main/kotlin/lacar/junilu/Day14.kt
+++ src/main/kotlin/lacar/junilu/Day14.kt
@@ -5,46 +5,48 @@ import kotlin.math.min
 /**
  * AoC 2015 - Day 14: Reindeer Olympics
  */
-class Day14(private val reindeerData: List<ReindeerData>, private val raceTime: Int) : Solution<Int> {
+class Day14(private val reindeerStats: List<ReindeerStats>, private val raceTime: Int) : Solution<Int> {
 
-    override fun part1() = reindeerData.maxOf { it.distanceFlownIn(raceTime) }
+    override fun part1() = reindeerStats.maxOf { it.distanceFlownIn(raceTime) }
 
     override fun part2(): Int {
-        val scores = raceByPointSystem()
-        return scores.values.maxOf { it }
+        raceByPointSystem()
+        return reindeerStats.maxOf { it.score }
     }
 
-    private fun raceByPointSystem(): Map<ReindeerData, Int> {
-        val scores = reindeerData.associateWith { 0 }.toMutableMap()
-        val distances = reindeerData.associateWith { it.distancesForEachSecondDuring(raceTime) }
-        (0 until raceTime).forEach { i ->
-            val maxDistanceSoFar = distances.maxOf { (_, progress) -> progress[i] }
-            distances.forEach { (reindeer, distances) ->
-                if (distances[i] == maxDistanceSoFar) scores[reindeer] = scores[reindeer]!!.inc()
-            }
+    private fun raceByPointSystem() {
+        reindeerStats.forEach { it.resetScore() }
+        (1..raceTime).forEach { raceSecond ->
+            val maxDistance = reindeerStats.maxOf { it.distanceFlownIn(raceSecond) }
+            reindeerStats.forEach { it.score(maxDistance, raceSecond) }
         }
-        return scores
     }
 
     companion object {
         fun using(input: List<String>, raceTime: Int) = Day14(
             input.map { line ->
                 val parts = line.split(" ")
-                ReindeerData(speed = parts[3].toInt(), flightTime = parts[6].toInt(), parts[13].toInt())
+                ReindeerStats(speed = parts[3].toInt(), flightTime = parts[6].toInt(), parts[13].toInt())
             },
             raceTime
         )
     }
 
-    data class ReindeerData(val speed: Int, val flightTime: Int, val restTime: Int) {
+    data class ReindeerStats(val speed: Int, val flightTime: Int, val restTime: Int) {
+
         private val cycleTime = flightTime + restTime
+
+        var score = 0
+
+        fun resetScore() { score = 0 }
+
+        fun score(distanceToBeat: Int, raceTime: Int) {
+            if (distanceFlownIn(raceTime) >= distanceToBeat) score++
+        }
 
         fun distanceFlownIn(raceTime: Int) = speed * secondsFlyingDuring(raceTime)
 
         private fun secondsFlyingDuring(raceTime: Int) =
             raceTime / cycleTime * flightTime + min(flightTime, raceTime % cycleTime)
-
-        fun distancesForEachSecondDuring(raceTime: Int): List<Int> =
-            (1..raceTime).map { time -> distanceFlownIn(time) }
     }
 }

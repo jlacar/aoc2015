4f1c7da1c79e3d5e96edaac8d181288aaf28cae8
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-28 19:54:08 -0400

    r - Brought back the mutableListOf() to initialize the fold(). Ran the code through the IntelliJ Profiler and neither version was flagged. Looks like the program spends most of its time (9ms) in the listOf() calls that create the original list to permute.

--- src/main/kotlin/lacar/junilu/Utils.kt
+++ src/main/kotlin/lacar/junilu/Utils.kt
@@ -12,30 +12,25 @@ val <T> List<T>.tail: List<T>
  * Adapted from Python: https://inventwithpython.com/recursion/chapter6.html and feedback
  * from https://www.reddit.com/r/Kotlin/comments/1edxill/seeing_some_strange_behavior_with_apply_scope/
  */
-fun <T> List<T>.permutations(): List<List<T>> {
+fun <T : Any> List<T>.permutations(): List<List<T>> {
     if (isEmpty()) return emptyList()
     if (size == 1) return listOf(this)
-    val allPerms = mutableListOf<List<T>>()
-    return tail.permutations()
-        .forEach { perm ->
-            (0..perm.size).mapTo(allPerms) { i ->
-                perm.subList(0, i) + head + perm.subList(i, perm.size)
-            }
-        }.let { allPerms }
+
+    return tail.permutations().fold(mutableListOf()) { acc, perm ->
+        (0..perm.size).mapTo(acc) { i ->
+            perm.subList(0, i) + head + perm.subList(i, perm.size)
+        }
+    }
 }
 
 fun main() {
     fun demo(aList: List<Any>) {
         val perms = aList.permutations()
-        println(
-            """${aList}.permutations (size=${perms.size}) = 
-            |${perms.joinToString("\n")}""".trimMargin()
-        )
-        println()
+        println(perms.size)
     }
     demo(emptyList())
     demo(listOf("oneThing"))
     demo(listOf('0', '1'))
     demo("abc".toList())
-    demo(listOf(1, 2, 3, 4))
+    demo(listOf(1, 2, 3, 4, 6, 7))
 }

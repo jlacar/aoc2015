740606e9db69e00e2c85e91dfd35df85ebdcf866
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-07-30 21:44:44 -0400

    F - Solved Day15, Part 1

--- src/main/kotlin/lacar/junilu/Day15.kt
+++ src/main/kotlin/lacar/junilu/Day15.kt
@@ -1,9 +1,27 @@
 package lacar.junilu
 
-class Day15(val ingredients: List<Ingredient>, val teaspoonsTotal: Int) : Solution<Int> {
-    
+import kotlin.math.max
+
+class Day15(private val ingredients: List<Ingredient>, private val teaspoonsTotal: Int) : Solution<Int> {
+
     override fun part1(): Int {
-        TODO("Not yet implemented")
+        val scores = mutableListOf(0)
+        val portions = proportions(ingredients.size, teaspoonsTotal).iterator()
+        while (portions.hasNext()) {
+            scores.add(cookieScore(portions.next()))
+        }
+        return scores.max()
+    }
+
+    fun cookieScore(portions: IntArray): Int {
+        val propertiesPart1 = listOf("capacity", "durability", "flavor", "texture")
+        val ingredientScores = portions.mapIndexed { i, teaspoons -> ingredients[i].scoreFor(teaspoons) }
+        val propertySums = propertiesPart1.map { property ->
+            ingredientScores.sumOf { score: Map<String, Int> ->
+                score.getOrDefault(property, 0)
+            }
+        }
+        return propertySums.fold(1) { acc, sum -> acc * max(0, sum) }
     }
 
     override fun part2(): Int {
@@ -13,19 +31,12 @@ class Day15(val ingredients: List<Ingredient>, val teaspoonsTotal: Int) : Soluti
     companion object {
         fun using(input: List<String>, teaspoonsTotal: Int): Day15 {
             val ingredients = input.map { line ->
-                val props = line
-                    .split(": ")[1].split(", ")
-                    .mapTo(mutableListOf()) { prop ->
-                        val (name, units) = prop.split(" ")
-                        name to units.toInt()
-                    }.toMap()
-
                 Ingredient(
-                    capacity = props["capacity"] ?: 0,
-                    durability = props["durability"] ?: 0,
-                    flavor = props["flavor"] ?: 0,
-                    texture = props["texture"] ?: 0,
-                    calories = props["calories"] ?: 0
+                    line.substringAfter(": ").split(", ")
+                        .mapTo(mutableListOf()) { prop ->
+                            val (name, units) = prop.split(" ")
+                            name to units.toInt()
+                        }.toMap()
                 )
             }
 
@@ -33,12 +44,23 @@ class Day15(val ingredients: List<Ingredient>, val teaspoonsTotal: Int) : Soluti
         }
     }
 
-    data class Ingredient(
-        val capacity: Int,
-        val durability: Int,
-        val flavor: Int,
-        val texture: Int,
-        val calories: Int) {
+    data class Ingredient(val props: Map<String, Int>) {
+        fun scoreFor(portionSize: Int) = props.map { (k, v) -> k to (v * portionSize) }.toMap()
     }
 }
 
+fun proportions(parts: Int, total: Int): Sequence<IntArray> = sequence {
+    val start = if (parts == 1) total else 0
+
+    for (i in (start..total)) {
+        val remaining = total - i
+        if (parts - 1 > 0) {
+            val remProps = proportions(parts - 1, remaining).iterator()
+            while (remProps.hasNext()) {
+                yield(intArrayOf(i) + remProps.next())
+            }
+        } else {
+            yield(intArrayOf(i))
+        }
+    }
+}
--- src/test/kotlin/lacar/junilu/Day15Test.kt
+++ src/test/kotlin/lacar/junilu/Day15Test.kt
@@ -8,14 +8,35 @@ private val puzzleInput = readResource("day15")
 
 class Day15Test {
     @Nested
-    inner class Samples {}
+    inner class Samples {
+        @Test
+        fun `Part 1 example`() {
+            assertEquals(62842880,
+                Day15.using(
+                    """
+                    Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+                    Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3  
+                    """.trimIndent().lines()
+                    , 100
+                ).part1()
+            )
+        }
+
+        @Test
+        fun `try proportions`() {
+            val props = proportions(3, 10).iterator()
+            while (props.hasNext()) {
+                println(props.next().joinToString(" "))
+            }
+        }
+    }
 
     @Nested
     inner class Solution {
 
         @Test
-        fun `Part 1 -`() {
-            assertEquals(0, Day15.using(puzzleInput, 100).part1())
+        fun `Part 1 - SOLVED`() {
+            assertEquals(222870, Day15.using(puzzleInput, 100).part1())
         }
     }
 }
